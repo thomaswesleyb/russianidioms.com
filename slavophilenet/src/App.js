a07e07a40dc112cf980e3706fc5f95773421ae0d
thomaswesleyb <weschess@yahoo.com>
2024-08-17 13:31:38 -0400

    Added styling and title card links

--- slavophilenet/src/App.js
+++ slavophilenet/src/App.js
@@ -6,6 +6,7 @@ import {Register} from "./Register";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 
+
 function App() {
     return (
         <Router>
--- slavophilenet/src/Main.js
+++ slavophilenet/src/Main.js
@@ -1,10 +1,18 @@
 import {React} from "react";
+import {Link} from "react-router-dom";
 
 export function Main() {
     return (
-        <div>
-            <h1>Slavophile.net</h1>
-            <p>Welcome to Slavophile.net, the place to learn about the Slavic world!</p>
+        <div class={"titleCard"}>
+            <h1>Russianidioms.com</h1>
+            <p>Welcome to Russianidioms.com, the place to learn all about Russian idioms!</p>
+            <nav>
+                <ul>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                    <li><Link to="/cards">Cards</Link></li>
+                </ul>
+            </nav>
         </div>
     );
 }

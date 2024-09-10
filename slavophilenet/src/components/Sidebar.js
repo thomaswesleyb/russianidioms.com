6e9c4ed8c7cf945a21b686de523e3942db5ce08e
thomaswesleyb <weschess@yahoo.com>
2024-09-10 17:17:23 -0500

    Removed empty sidebar items

--- slavophilenet/src/components/Sidebar.js
+++ slavophilenet/src/components/Sidebar.js
@@ -6,13 +6,8 @@ export function Sidebar() {
     return (
         <div className="sidebar">
             <ul>
-                <li><a href="#section1">Daily idiom</a></li>
                 <li><Link to="/idioms">Idioms</Link></li>
                 <li><Link to="/cards">Flashcards</Link></li>
-                <li><a href="#section4">Quiz</a></li>
-                <li><a href="#section5">External Resources</a></li>
-                <li><a href="#section6">Grammar</a></li>
-                <li><a href="#section7">Vocabulary</a></li>
                 <li><Link to="/new-idiom">Submit Idiom</Link></li>
                 <li><Link to="/my-idioms">My Idioms</Link></li>
             </ul>

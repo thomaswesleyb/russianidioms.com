85e4f367d6ffd33f7ddde9581e810759443c5ed6
thomaswesleyb <weschess@yahoo.com>
2024-09-10 19:24:52 -0500

    Added responsiveness to sidebar

--- slavophilenet/src/components/Sidebar.js
+++ slavophilenet/src/components/Sidebar.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style/Sidebar.css';
 import {Link} from "react-router-dom";
 
 export function Sidebar() {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsActive(!isActive);
+    };
+
     return (
-        <div className="sidebar">
-            <ul>
-                <li><Link to="/idioms">Idioms</Link></li>
-                <li><Link to="/cards">Flashcards</Link></li>
-                <li><Link to="/new-idiom">Submit Idiom</Link></li>
-                <li><Link to="/my-idioms">My Idioms</Link></li>
-            </ul>
+        <div>
+            <div>
+                <div className="sidebar-toggle" onClick={toggleSidebar}>
+                    ☰ Menu
+                </div>
+            </div>
+            <div className={`sidebar ${isActive ? 'active' : ''}`}>
+                <ul>
+                    <li><Link to="/idioms">Idioms</Link></li>
+                    <li><Link to="/cards">Flashcards</Link></li>
+                    <li><Link to="/new-idiom">Submit Idiom</Link></li>
+                    <li><Link to="/my-idioms">My Idioms</Link></li>
+                </ul>
+            </div>
         </div>
     );
 }

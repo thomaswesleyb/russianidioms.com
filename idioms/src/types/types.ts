2cb5ed24fe2f121aad03a779c3431151c7b25691
thomaswesleyb <twesleybailey@gmail.com>
2024-12-04 13:09:07 -0600

    Fix build errors

--- idioms/src/types/types.ts
+++ idioms/src/types/types.ts
@@ -1,10 +1,30 @@
 export interface Idiom {
     id: string;
+    idiom: string;
+    english: string;
+    example?: string;
+    submittedBy?: string;
+    approvalStatus?: string;
+}
+
+export interface ApiIdiom {
+    id: string;
     data: {
         idiom: string;
         english: string;
         example?: string;
         submittedBy?: string;
         approvalStatus?: string;
-    }
+    };
+}
+
+export function transformApiIdiom(apiIdiom: ApiIdiom): Idiom {
+    return {
+        id: apiIdiom.id,
+        idiom: apiIdiom.data.idiom,
+        english: apiIdiom.data.english,
+        example: apiIdiom.data.example,
+        submittedBy: apiIdiom.data.submittedBy,
+        approvalStatus: apiIdiom.data.approvalStatus,
+    };
 }
